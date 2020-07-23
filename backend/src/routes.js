1e27e319f374ca4664de8361641cf8b67049cc95
Douglas Sena <dsena.m@hotmail.com>
2020-07-22 23:41:27 -0300

    User Controller,  store ok , password hash ok

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'hello world' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
