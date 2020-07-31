f009f7e73fd16fa8319488a9a38f1f8d10571f07
Douglas Sena <dsena.m@hotmail.com>
2020-07-31 16:42:43 -0300

    Agendamentos concluidos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = Router();
 const upload = multer(multerConfig);
@@ -18,5 +19,7 @@ routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 
 export default routes;
